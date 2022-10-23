882bebe3740ead223a38ed724a496d5ac2aeac2b
Nimit Bhatia <nimitbhatia@192.168.1.2>
2022-10-23 09:19:46 +0530

    Data received and read on the backend

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
+
 app = Flask(__name__)
 
-@app.route('/getUUIDList', methods = ['GET'])
+@app.route('/getUUIDList', methods = ['POST'])
 def getUUIDList():
-    print('Inside getUUIDList')
-    return jsonify({"Hello": "World"})
+    data = request.get_json()
+    print(data)
+    return jsonify({"Yes": "Worked"})
 
 if(__name__ == '__main__'):
     print('The server is running...')
-    app.run(debug = False)
+    app.run(port = 4000, debug = False)
